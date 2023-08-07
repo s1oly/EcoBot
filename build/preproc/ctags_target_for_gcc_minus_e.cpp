353e3b641df92dac9d4bb54807ab0d777225bfd5
s1oly <90860963+s1oly@users.noreply.github.com>
2023-08-06 21:04:02 -0400

    Added right turn method to test

--- build/preproc/ctags_target_for_gcc_minus_e.cpp
+++ build/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -5,6 +5,43 @@
 
 Servo myservo;
 
+AF_DCMotor front_leftmotor(1);
+AF_DCMotor front_rightmotor(2);
+AF_DCMotor back_rightmotor(3);
+AF_DCMotor back_leftmotor(4);
+
+void runMotors(int speed){
+  front_leftmotor.setSpeed(speed);
+  front_rightmotor.setSpeed(speed);
+  back_rightmotor.setSpeed(speed);
+  back_leftmotor.setSpeed(speed);
+
+  front_leftmotor.run(2);
+  front_rightmotor.run(1);
+  back_rightmotor.run(1);
+  back_leftmotor.run(2);
+}
+
+void runServo(int position){
+  myservo.write(position);
+  delay(15);
+}
+
+void turnLeft(int duration, int speed1, int speed2){
+front_rightmotor.setSpeed(speed1);
+back_rightmotor.setSpeed(speed1);
+front_leftmotor.setSpeed(speed2);
+back_leftmotor.setSpeed(speed2);
+front_rightmotor.run(1);
+back_rightmotor.run(1);
+front_leftmotor.run(2);
+back_leftmotor.run(2);
+delay(duration);
+front_rightmotor.run(4);
+back_rightmotor.run(4);
+front_leftmotor.run(4);
+back_leftmotor.run(4);
+}
 
 void setup()
 {
--- build/sketch/EcoBot.ino.cpp
+++ build/sketch/EcoBot.ino.cpp
@@ -6,12 +6,55 @@
 
 Servo myservo; 
 
+AF_DCMotor front_leftmotor(1);
+AF_DCMotor front_rightmotor(2);
+AF_DCMotor back_rightmotor(3);
+AF_DCMotor back_leftmotor(4);
 
-#line 8 "/Users/s1oly/Documents/GitHub/EcoBot/EcoBot/EcoBot.ino"
+#line 12 "/Users/s1oly/Documents/GitHub/EcoBot/EcoBot/EcoBot.ino"
+void runMotors(int speed);
+#line 24 "/Users/s1oly/Documents/GitHub/EcoBot/EcoBot/EcoBot.ino"
+void runServo(int position);
+#line 29 "/Users/s1oly/Documents/GitHub/EcoBot/EcoBot/EcoBot.ino"
+void turnLeft(int duration, int speed1, int speed2);
+#line 45 "/Users/s1oly/Documents/GitHub/EcoBot/EcoBot/EcoBot.ino"
 void setup();
-#line 13 "/Users/s1oly/Documents/GitHub/EcoBot/EcoBot/EcoBot.ino"
+#line 50 "/Users/s1oly/Documents/GitHub/EcoBot/EcoBot/EcoBot.ino"
 void loop();
-#line 8 "/Users/s1oly/Documents/GitHub/EcoBot/EcoBot/EcoBot.ino"
+#line 12 "/Users/s1oly/Documents/GitHub/EcoBot/EcoBot/EcoBot.ino"
+void runMotors(int speed){
+  front_leftmotor.setSpeed(speed);
+  front_rightmotor.setSpeed(speed);
+  back_rightmotor.setSpeed(speed);
+  back_leftmotor.setSpeed(speed);
+  
+  front_leftmotor.run(BACKWARD);
+  front_rightmotor.run(FORWARD);
+  back_rightmotor.run(FORWARD);
+  back_leftmotor.run(BACKWARD);
+}
+
+void runServo(int position){
+  myservo.write(position);
+  delay(15);
+}
+
+void turnLeft(int duration, int speed1, int speed2){
+front_rightmotor.setSpeed(speed1);
+back_rightmotor.setSpeed(speed1);
+front_leftmotor.setSpeed(speed2);
+back_leftmotor.setSpeed(speed2);
+front_rightmotor.run(FORWARD);
+back_rightmotor.run(FORWARD);
+front_leftmotor.run(BACKWARD);
+back_leftmotor.run(BACKWARD);
+delay(duration);
+front_rightmotor.run(RELEASE);
+back_rightmotor.run(RELEASE);
+front_leftmotor.run(RELEASE);
+back_leftmotor.run(RELEASE);
+}
+
 void setup()
 {
 	
